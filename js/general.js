4ffe7eece88e1a92e406297a3c66fdf8cc1dbc03
dschkalee <dschkalee@isa-hamburg.com>
2015-10-09 18:24:34 +0200

    fixed scrolling-height

--- js/general.js
+++ js/general.js
@@ -1,17 +1,28 @@
 var owfScenes = 5;
-var viewportHeight = $(window).height();
-var viewportWidth = $(window).width();
-var skr = "";
+var viewportHeight = 0;
+var viewportWidth = 0;
+var skr;
 
-/**
- * Init skroller
- * @return {[type]}
- */
-function initSkrollr() {
+function initScenes() {
 	var body = $('body');
+	var bodyHeight = 0;
 
-	if(window.innerWidth < 768) {
-		if(skr) skr.destroy();
+	if(skr) skr.destroy();
+
+	viewportHeight = innerHeight;
+	viewportWidth = innerWidth;
+
+	$('.page').each(function() {
+		bodyHeight += Math.max(viewportHeight, $(this).children('.container').height());
+	});
+
+	body.css("height", bodyHeight);
+
+	//set start page height
+	$("#page-start").css("height", viewportHeight + "px");
+
+	if (window.innerWidth < 768) {
+		if (skr) skr.destroy();
 		body.addClass('mobile');
 
 		return;
@@ -19,10 +30,31 @@ function initSkrollr() {
 
 	body.removeClass('mobile');
 
+	//
 	var constantsData = {};
+	var curHeight = 0;
+	var curP = 0;
 
 	for (i = 1; i <= owfScenes; i++) {
-		constantsData["owf" + i] = Math.floor(viewportHeight * i);
+		var page = $('.page:eq(' + (i - 1) + ')');
+		var height = Math.max(page.children('.container').height(), viewportHeight);
+		var pHeight = Math.round(height / viewportHeight * 100);
+
+		page.attr('data-_owf' + (i - 1), 'height:' + pHeight + '%');
+
+		if (pHeight > 100) {
+			var pStart = curP;
+			var pEnd = pStart + pHeight;
+
+			page.children('.container')
+				.attr('data-' + pStart + 'p', 'transform:translate(0, 0%);')
+				.attr('data-' + pEnd + 'p', 'transform:translate(0, -100%);')
+		}
+
+		curHeight += height;
+		curP += pHeight;
+
+		constantsData["owf" + i] = curHeight;
 	}
 
 	skr = skrollr.init({
@@ -32,32 +64,6 @@ function initSkrollr() {
 	});
 }
 
-function initScenes(resize) {
-	var pageInfo = $("#page-info");
+$(document).ready(initScenes);
 
-	if (resize) {
-		viewportHeight = pageInfo.height();
-		viewportWidth = pageInfo.width();
-		skr.destroy();
-	}
-
-	var _bodyHeight = Math.floor(viewportHeight * owfScenes);
-	_bodyHeight += 650;
-	$("body").css("height", _bodyHeight);
-
-	//set start page height
-	$("#page-start").css("height", viewportHeight + "px");
-
-	//skrollr
-	initSkrollr();
-}
-
-$(document).ready(function () {
-	initScenes(false);
-});
-
-window.onresize = function () {
-	if(!skr) initSkrollr();
-
-	initScenes(true);
-};
+window.onresize = initScenes;
